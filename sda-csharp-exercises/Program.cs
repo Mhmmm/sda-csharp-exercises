838d27a8df302c6ed83a47a6e7499405239b0486
Mhmmm <renusek91@gmail.com>
2021-11-07 11:30:28 +0100

    Zrobiono zadanie E05

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,6 +6,32 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Factorial(5));
+            Console.WriteLine(FactorialRecursive(5));
         }
+        public static int Factorial(int number)
+        {
+            int result = 1;
+            for (int i = number; i > 1; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        public static int FactorialRecursive(int number)
+        {
+            int result = 1;
+            result *= number;
+
+            if (number > 1)
+            {
+                return result *= FactorialRecursive(number - 1);
+            }
+
+            return result;
+        }
+
     }
 }

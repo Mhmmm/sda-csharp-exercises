577bee77b9afbb1cef2bbc47a93a31312840e9df
Mhmmm <renusek91@gmail.com>
2021-11-07 11:03:21 +0100

    Rozwiązano zadanie E04

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,6 +6,24 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            DrawTriangle(3);
+            Console.WriteLine();
+            DrawTriangle(5);
+        }
+
+        public static void DrawTriangle(int lines, char sign = '*')
+        {
+            string line = "";
+            for (int i = 1; i <= lines; i++)
+            {
+                line += sign.ToString();
+                Console.WriteLine(line);
+            }
+            for (int i = lines; i > 0; i--)
+            {
+                line = line.Remove(i-1, 1);
+                Console.WriteLine(line);
+            }
         }
     }
 }

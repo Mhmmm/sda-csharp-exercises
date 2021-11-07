7ca3ad0c3f9e52a63de51a9f25ab1a5d9e46da87
Mhmmm <renusek91@gmail.com>
2021-11-07 10:08:27 +0100

    Zrobiono zadanie E02

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,6 +6,26 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Pow(2, 5);
+            Pow(2, 0);
+            Pow(2, -5);
+        }
+
+        public static void Pow(int number, int power)
+        {
+            int result = 1; //default result for power == 0
+
+            if (power < 0)
+            {
+                Console.WriteLine("Power can't be lower than zero.");
+                return;
+            }
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            Console.WriteLine(result);
         }
     }
 }

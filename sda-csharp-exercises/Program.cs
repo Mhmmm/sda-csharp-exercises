c3eb78af25712ed875743199584d0bcc3b3e2762
Mhmmm <renusek91@gmail.com>
2021-11-07 13:27:54 +0100

    Ukończono zadanie E07

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,6 +6,49 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            string str = "Example string";
+            Console.WriteLine("Input: " + str);
+            str = Leetify(str);
+            Console.WriteLine("Leetified: " + str);
+            Console.WriteLine("Unleetified: " + Unleetify(str));
+        }
+
+        public static string Leetify(string str)
+        {
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+            string result = str.ToLower();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = 0; j < naturalLetters.Length; j++)
+                {
+                    if (result[i] == naturalLetters[j])
+                    {
+                        result = result.Replace(result[i], leetLetters[j]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static string Unleetify(string str)
+        {
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+            string result = str;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = 0; j < leetLetters.Length; j++)
+                {
+                    if (result[i] == leetLetters[j])
+                    {
+                        result = result.Replace(result[i], naturalLetters[j]);
+                    }
+                }
+            }
+            return result;
         }
     }
 }

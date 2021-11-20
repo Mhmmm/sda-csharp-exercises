a9814044f94919bf0c85d54c9caaa6239ac580b4
Mhmmm <renusek91@gmail.com>
2021-11-20 09:29:54 +0100

    Rozpoczęto E12

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -7,5 +7,23 @@ namespace sda_csharp_exercises
         static void Main(string[] args)
         {
         }
+        public static void CountWords(string phrase)
+        {
+            string[] words = phrase.Split(' ');
+            //może string[][]?
+            int maxLength = 0;
+
+            foreach (string word in words)
+            {
+                maxLength = Math.Max(maxLength, word.Length); //sprawdzanie najdłuższego wyrazu
+            }
+
+            for (int i = 1; i <= maxLength; i++)
+            {
+                Console.WriteLine($"długość {i} - WYRAZY TUTAJ - liczba słów - ILOŚĆ SŁÓW TUTAJ");
+            }
+
+            Console.WriteLine();
+        }
     }
 }

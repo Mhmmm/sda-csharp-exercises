c9e9405e507011031c5713c67cb2786f73a6bbc1
Mhmmm <renusek91@gmail.com>
2021-11-20 11:08:01 +0100

    Implemented basic inheritance

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,6 +6,29 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            //Person person = new Person();
+            //person.WhoAmI();
+            //person.LastName = "Jan";
+            //person.FirstName = "Kowalski";
+            //person.YearOfBirth = 1981;
+            //person.WhoAmI();
+            //Console.WriteLine(person.IsAdult());
+
+            //Person person = new Person()
+            //{
+            //    FirstName = "Jan",
+            //    LastName = "Kowalski",
+            //    YearOfBirth = 1981
+            //};
+            //person.WhoAmI();
+            //Console.WriteLine(person.IsAdult());
+            Employee employee = new Employee();
+            employee.FirstName = "Jan";
+            employee.LastName = "Kowalski";
+            employee.YearOfBirth = 1981;
+            employee.Salary = 5000;
+
+            employee.WhoAmI();
         }
     }
 }

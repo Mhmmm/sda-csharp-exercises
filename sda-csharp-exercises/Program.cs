d511bc5c013c0a9f8fe00a8a0df0b47878da68fc
Mhmmm <renusek91@gmail.com>
2021-11-20 13:47:18 +0100

    Hiding base class members and using new

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -7,9 +7,12 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Person person0 = new Person("Joanna", "Kowalska", 1982);
+            person0.WhoAmI();
+            Person person = new Employee("Adam", "Nowak", 2000, 6000);
+            person.WhoAmI();
             Employee employee = new Employee("Jan", "Kowalski", 1981, 5000);
-
-            employee.WhoAmIWithSalary();
+            employee.WhoAmI();
         }
     }
 }

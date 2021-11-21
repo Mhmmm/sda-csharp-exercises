179a5a411ee11cc362310755e4e2ec587f48ef34
Mhmmm <renusek91@gmail.com>
2021-11-21 13:19:50 +0100

    zrobiono E23

---
+++ sda-csharp-exercises/LineSegment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class LineSegment
+    {
+        public double point1x = 0;
+        public double point1y = 0;
+        public double point2x = 0;
+        public double point2y = 0;
+
+        public LineSegment(MyPoint point1, MyPoint point2)
+        {
+            point1x = point1.X;
+            point1y = point1.Y;
+            point2x = point2.X;
+            point2y = point2.Y;
+        }
+
+        public LineSegment(double x1, double y1, double x2, double y2)
+        {
+
+            point1x = x1;
+            point1y = y1;
+            point2x = x2;
+            point2y = y2;
+        }
+
+        public double GetLength()
+        {
+            return Math.Sqrt(Math.Pow(point2x - point1x, 2) + Math.Pow(point2y - point1y, 2));
+        }
+    }
+}
--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -6,6 +6,13 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            MyPoint point1 = new MyPoint(1, 2);
+            MyPoint point2 = new MyPoint(5, 4);
+            LineSegment line1 = new LineSegment(point1, point2);
+            LineSegment line2 = new LineSegment(5, 4, 1, 2);
+
+            Console.WriteLine(line1.GetLength());
+            Console.WriteLine(line2.GetLength());
         }
     }
 }
